4ffbf914e9d07dc9e4fe6b182da43701ec677ca7
Dave Simel <davesimel@gmail.com>
2015-05-20 13:59:03 -0400

    almost done with extra credit

--- server.rb
+++ server.rb
@@ -6,23 +6,33 @@ file = File.read('roster.json')
 teams_info = JSON.parse(file)
 
 team_names = []
-positions = []
+positions = {}
 
 teams_info.each do |team, info|
   team_names << team
-  positions << info
+  info.each do |position, name|
+    positions[position] = []
+  end
 end
 
+teams_info.each do |team, info|
+  team_names << team
+  info.each do |position, name|
+    positions[position] << name
+  end
+end
 
-get "/teams" do
-  erb :index, locals: { team_names: team_names }
+print teams_info
+print positions
+
+get "/" do
+  erb :index, locals: { team_names: team_names, positions: positions }
 end
 
 get "/teams/:team_name" do
-
-  erb :show, locals: { team_name: params[:team_name], teams_info: teams_info }
+  erb :team, locals: { team_name: params[:team_name], teams_info: teams_info }
 end
 
-# get "/teams/:position" do
-#   erb :show, locals: { team_name: params[:team_name], teams_info: teams_info }
-# end
+get "/positions/:position" do
+  erb :positions, locals: { position: params[:position], positions: positions }
+end
